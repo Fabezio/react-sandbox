9192392061fb390150f7a6b20614eac4e693903e
fabezio <fabezio@outlook.fr>
2021-04-20 20:33:41 +0200

    factorisation formulaire

--- src/containers/section.js
+++ src/containers/section.js
@@ -4,16 +4,17 @@ function Section () {
   const p = str => <p>{str}</p>
   const li = str => <li key={str}>{str}</li>
   const ul = ls => <ul>{ls}</ul>
-  const list = "Moi je suis une liste".split(" ")
+  // const mks = (mk, str) => Object.toHTML(`<${mk}>{str}</${mk}>`)
+  const list = 'Moi je suis une liste'.split(' ')
   const el = list.map(word => li(word))
   return (
     <div>
-      
+      {/* {mks('h2', 'markup test')} */}
       <Form />
-      {p('un paragraphe')}
+      {/* {p('un paragraphe')}
       {p('un autre paragraphe')}
       {ul(el)}
-      {p(list.join('_')+'_avec_des_tirets')}
+      {p(list.join('_') + '_avec_des_tirets')} */}
     </div>
   )
 }
--- src/forms/form.js
+++ src/forms/form.js
@@ -2,19 +2,36 @@ import { useState } from 'react'
 
 function Form () {
   const [inputValue, setInputValue] = useState('')
-  const [msg, commitValue] = useState('')
+  const [msg, submitValue] = useState('')
   //   let msg = ''
+  const input = (type = 'text', label, val, setVal) => {
+    return (
+      <input
+        type={type}
+        placeholder={label}
+        value={val}
+        onChange={e => setVal(e.target.value)}
+      />
+    )
+  }
+  // if (msg.length > 0) {
+  //   msg.preventDefault()
+  // }
   return (
     <div>
-      <form>
-        <input
-          type='text'
-          value={inputValue}
-          onChange={e => setInputValue(e.target.value)}
-          placeholder='entre un truc'
-        />
-        <button onClick={() => commitValue(inputValue)}>Envoyer</button>
-      </form>
+      {/* <form>
+        </form> */}
+      {/* <input
+        type='text'
+        value={inputValue}
+        onChange={e => setInputValue(e.target.value)}
+        placeholder='entre un truc'
+      /> */}
+      {input('date', 'date de naissance?', inputValue, setInputValue)}
+      {/*
+        {input('number', 'Numero de téléphone: ', inputValue, setInputValue)}
+      */}
+      <button onClick={() => submitValue(inputValue)}>Envoyer</button>
       <p>{msg}</p>
     </div>
   )
